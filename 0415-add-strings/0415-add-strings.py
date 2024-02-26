509ce22fa5a0c4e42b105fc05e395f270cb70a8d
Mikiyas Alemayehu <153812380+zemike47@users.noreply.github.com>
2024-02-26 16:03:10 +0300

    Time: 15 ms (85.54%), Space: 11.8 MB (91.16%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        intNum1 = int(num1)
+        intNum2 = int(num2)
+        
+        sum = intNum1 + intNum2
+        
+        sum = str(sum)
+        
+        return sum
