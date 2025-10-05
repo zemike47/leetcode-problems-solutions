a92b003261ddc56c17c8a67ffc04d6534e12e96a
Mikiyas Alemayehu <153812380+zemike47@users.noreply.github.com>
2025-10-05 18:25:43 +0300

    Time: 0 ms (100%), Space: 18.2 MB (39.52%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,28 +1,24 @@
 class Solution:
-    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
-        # Make sure nums1 is the smaller array
-        if len(nums1) > len(nums2):
-            nums1, nums2 = nums2, nums1
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        nums = nums1 + nums2
         
-        m, n = len(nums1), len(nums2)
-        left, right = 0, m
+        nums.sort()
+
+        length = len(nums)
+
+        if length % 2 == 0:
+            i = int((length / 2) - 1)
+            median = (nums[i] + nums[i+1]) / 2
+
+            return float(median)
         
-        while left <= right:
-            i = (left + right) // 2
-            j = (m + n + 1) // 2 - i
-            
-            nums1_left = nums1[i - 1] if i > 0 else float('-inf')
-            nums1_right = nums1[i] if i < m else float('inf')
-            nums2_left = nums2[j - 1] if j > 0 else float('-inf')
-            nums2_right = nums2[j] if j < n else float('inf')
-            
-            if nums1_left <= nums2_right and nums2_left <= nums1_right:
-                # Correct partition found
-                if (m + n) % 2 == 1:
-                    return max(nums1_left, nums2_left)
-                else:
-                    return (max(nums1_left, nums2_left) + min(nums1_right, nums2_right)) / 2
-            elif nums1_left > nums2_right:
-                right = i - 1
-            else:
-                left = i + 1
+        else:
+            i = int((length // 2))
+            median = nums[i]
+
+            return float(median)
+
+
+        
+
+        
